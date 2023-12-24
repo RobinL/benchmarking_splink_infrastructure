4143c22b825b21f961caec3001af67da4c97e77a
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-12-24 13:42:24 +0000

    Refactor download_s3_bucket_contents function to support downloading files from a specific folder

--- analyse.py
+++ analyse.py
@@ -12,13 +12,22 @@ def create_and_clear_directory(directory):
     os.makedirs(directory)
 
 
-def download_s3_bucket_contents(bucket_name, local_directory):
+def download_s3_bucket_contents(*, bucket_name, local_directory, s3_folder_path):
     s3 = boto3.client("s3")
+    prefix = s3_folder_path.rstrip("/") + "/"  # Ensure the folder path ends with '/'
+
     try:
-        for obj in s3.list_objects_v2(Bucket=bucket_name)["Contents"]:
-            s3_file_name = obj["Key"]
-            local_file_name = os.path.join(local_directory, s3_file_name)
-            s3.download_file(bucket_name, s3_file_name, local_file_name)
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if "Contents" in response:
+            for obj in response["Contents"]:
+                s3_file_name = obj["Key"]
+                if not s3_file_name.endswith("/"):  # Skip directories
+                    local_file_name = os.path.join(
+                        local_directory, os.path.basename(s3_file_name)
+                    )
+                    s3.download_file(bucket_name, s3_file_name, local_file_name)
+        else:
+            print("No files found in the specified folder")
     except NoCredentialsError:
         print("Credentials not available")
     except KeyError:
@@ -26,11 +35,17 @@ def download_s3_bucket_contents(bucket_name, local_directory):
 
 
 bucket_name = "robinsplinkbenchmarks"
+s3_directory = "pytest_benchmark_results"
 local_directory = "benchmarking_json"
 
-create_and_clear_directory(local_directory)
-download_s3_bucket_contents(bucket_name, local_directory)
 
+create_and_clear_directory(local_directory)
+
+download_s3_bucket_contents(
+    bucket_name=bucket_name,
+    local_directory=local_directory,
+    s3_folder_path=s3_directory,
+)
 
 conn = duckdb.connect()
 
