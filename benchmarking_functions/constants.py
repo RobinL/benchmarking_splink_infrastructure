0546c0f576b448fbd5ef83a22e56c5dc7b4058cc
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-01-05 09:01:56 +0000

    Update INSTANCE_TYPE in constants.py

--- benchmarking_functions/constants.py
+++ benchmarking_functions/constants.py
@@ -7,7 +7,8 @@ CLOUDWATCH_IAM_POLICY_NAME = "CloudWatchAccessRobinSplinkBenchmarks"
 EC2_IAM_INSTANCE_PROFILE_NAME = "EC2RobinBenchmarksInstanceProfile"
 
 IMAGEID = "ami-05cae8d4948d6f5b7"  # arm64
-INSTANCE_TYPE = "c6g.16xlarge"  # arm64
+# INSTANCE_TYPE = "c6g.16xlarge"  # arm64
+INSTANCE_TYPE = "c6g.xlarge"  # arm64
 
 # INSTANCE_TYPE = "c5.xlarge"  # x86_64
 # IMAGEID = "ami-0cfd0973db26b893b"  # x86_64
